1a5b73a218e87fd918bc4e6275a2e637b72e4600
Anka <runner@sat12-bq153-feb7c461-135b-4d4d-9b95-99d153832e28-3EE0D7E2227F.local>
2026-02-27 09:17:49 +0000

    update to newer version

--- Formula/macos-hid-inspector.rb
+++ Formula/macos-hid-inspector.rb
@@ -1,15 +1,15 @@
 class MacosHidInspector < Formula
-  version '0.2.2'
+  version '0.2.3'
   homepage 'https://github.com/masawada/macos-hid-inspector'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/masawada/macos-hid-inspector/releases/download/v0.2.2/inspect-hid_v0.2.2_darwin_arm64.tar.gz'
-      sha256 'cdcffa8959567dc3a9c49061638d6c32247f3a3379b68a690af48e871bc10e64'
+      url 'https://github.com/masawada/macos-hid-inspector/releases/download/v0.2.3/inspect-hid_v0.2.3_darwin_arm64.tar.gz'
+      sha256 '379881bbfd259281c41028073640fd64687f9a181c561f9d9ba1cd33b1062211'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/masawada/macos-hid-inspector/releases/download/v0.2.2/inspect-hid_v0.2.2_darwin_amd64.tar.gz'
-      sha256 '087ff6795aa08ceb9cee029c08ddaf87618953db7081aa61f7583c082df7fda4'
+      url 'https://github.com/masawada/macos-hid-inspector/releases/download/v0.2.3/inspect-hid_v0.2.3_darwin_amd64.tar.gz'
+      sha256 '7aa7f7a1fdcd3344367f15d77720ff193a928a57e27c3a947ccb5718f41f197f'
     end
   end
 
